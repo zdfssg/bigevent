9b5bfaa0d33a0c0b9f09b68a626d9866d4ca3655
zdf <1902176817@qq.com>
2021-01-28 11:05:45 +0800

    修改了部分代码

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,7 +7,8 @@ $(function () {
     })
 
     // 从layui中获取form对象
-    var form = layui.form
+    var form = layui.form;
+    var layer = layui.layer;
     // 通过form.verify()自定义校验规则
     // 自定义密码校验规则
     form.verify({
@@ -16,10 +17,33 @@ $(function () {
         // 校验两次密码是否一致的规则
         repwd: function (value) {
             // 通过形参拿到的是确认密码框中的内容
-            var pwd = $('.reg-box [name=password]').val()
+            var pwd = $('#reg [name=password]').val()
             if (pwd !== value) {
                 return '两次密码不一致！'
             }
         }
-    })
+    });
+
+    // 监听注册表单提交事件
+    $('#reg').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            method: 'post',
+            data: {
+                username: $('#reg [name=username]').val(),
+                password: $('#reg [name=password]').val(),
+            },
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                layer.msg('注册成功', {
+                    icon: 1,
+                    time: 1000,
+                }, function () {
+                    $('#link-login').click();
+                })
+            }
+        })
+    });
+
 })
